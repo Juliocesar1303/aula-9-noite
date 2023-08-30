948992d67ccf7497e3ef8dc87217fa7ac739b1c2
Juliocesar1303 <142456148+Juliocesar1303@users.noreply.github.com>
2023-08-29 21:44:13 -0300

    Create aula-9.js

---
+++ aula-9.js
@@ -0,0 +1,99 @@
+// let nome = prompt("qual é seu nome");
+// let cor = prompt("qual é sua cor favorita");
+
+// console.log(`A cor favorita de ${nome} é ${cor}`);
+// console.log("A cor favorita de" + nome + " é " + cor);
+
+// const frase = "SalveeeE!";
+
+// const fraseMinuscula = frase.toLowerCase();
+
+// const frase = "SalveeeE!"
+
+// const fraseMa = frase.toUpperCase();
+
+// const email = "mika@gmail.com";
+// console.log(emai.trim());
+
+// const frase="hoje comi cenoura"
+// frase.includes("cenoura") //true
+// frase.includes("batata") //false
+
+// const frase = "hoje comi cenoura, e adoro cenoura";
+// const novafrase = frase.replaceAll("cenoura","batata");
+
+// console.log(frase);
+// console.log(novafrase);
+
+
+ // let frase = prompt("digite uma frase");
+
+ // frase = frase.toLocaleUpperCase();
+ // frase = frase.replaceAll("o", "i");
+
+// console.log(frase);
+// console.log(frase.length);
+
+// const listaDeCompras = ["batata", "alface", "queijo"]
+
+// const listaDeNumerosMega = [2, 13, 26, 35, 41, 60 ]
+
+// const meuArray = ["banana" , 15, true]
+
+// const listaDeCompras = ["Abacate", "Banana", "tomate"]
+// const segundoItem = listaDeCompras[2] tomate
+
+
+// const dcachorro = ["pitibull", "Rask", "pinther", "Doberman", "pug"]
+// const dog = Number(prompt("de 0 a 4"));
+// console.log(cachorro(dog));
+
+// const pokemon = ["bulbasauro", "squitle", "charmander"]
+// console.log(pokemon.length) //3
+
+// const seriesBoas = ["the Big Bang theory", "Brooklyn Nine-nine"]
+
+// seriesBoas.includes("The Big Bang Theory") // true
+// seriesBoas.includes("Game of thrones") // false
+
+// const numeros = [1, 2, 3]
+
+// numeros.push(4)
+// console.log(numeros) // [1, 2, 3, 4]
+
+// numeros.push(5, 6, 7)
+// console.log(numeros) // [1, 2, 3, 4, 5, 6, 7]
+
+// const meuPeixes = ["palhaço", "mandarim", "esturjão"]
+
+// meusPeixes.pop()
+// console.log(meusPeixes) // ["palhaço", "mandarim"]
+
+
+
+// const letras =["A", "B", "C", "D", "E", "F", "G", "H"]
+// // indices (i)  0    1    2    3    4    5    6    7
+
+// letras.splice(2, 1)
+// //     letras = ["A", "B", "D", "E", "F", "G" "H"]
+// //    (i)         0    1    2    3    4    5   6
+
+// letras.splices(2, 1)
+
+// letras.splices(3, 2)
+
+// const numeros = [1, 2, 3, 4, 5, 6]
+
+// console.log(numeros.length)
+
+// numeros.push(7)
+
+// numeros.splice(3, 2)
+
+// console.log(numeros.length)
+
+
+
+
+
+
